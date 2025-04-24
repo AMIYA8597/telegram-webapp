cbb7137ec18a6b7ce946c634d61c7c6a6709b826
AMIYA8597 <chowdhuryamiya41@gmail.com>
2025-04-24 18:46:30 +0530

    web demo telegram app

--- Bot/bot.js
+++ Bot/bot.js
@@ -3,7 +3,7 @@ const TOKEN = "8047277273:AAG3pgsTwJ9-P9hWc6f6p7p9gKSEZaveV7g";
 const bot = new Telegraf(TOKEN);
 
 // const web_link = "https://celebrated-torte-184681.netlify.app/";
-const web_link = "https://telegram-web-tlgrm.netlify.app/";
+const web_link = "https://telegram-webapp-gules.vercel.app/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
